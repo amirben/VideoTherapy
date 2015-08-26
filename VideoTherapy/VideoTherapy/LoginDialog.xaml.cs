0f11610fa88f3369d40dada4223b2ad5f556822f
amirben <amir.ben@gmail.com>
2015-08-27 00:20:34 +0300

    login page

---
+++ VideoTherapy/VideoTherapy/LoginDialog.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace VideoTherapy
+{
+    /// <summary>
+    /// Interaction logic for LoginDialog.xaml
+    /// </summary>
+    public partial class LoginDialog : UserControl
+    {
+        private UIElement _parent;
+        private bool _hideRequest = false;
+
+        public LoginDialog()
+        {
+            InitializeComponent();
+            Visibility = Visibility.Visible;
+        }
+
+
+        public void SetParent(UIElement parent)
+        {
+            _parent = parent;
+
+        }
+        private void LoginBtn_Click(object sender, RoutedEventArgs e)
+        {
+            HideHandlerDialog();
+        }
+
+        public void ShowHandlerDialog()
+        {
+            Visibility = Visibility.Visible;
+
+            //_parent.IsEnabled = false;
+
+            _hideRequest = false;
+            while (!_hideRequest)
+            {
+                // HACK: Stop the thread if the application is about to close
+                if (this.Dispatcher.HasShutdownStarted ||
+                    this.Dispatcher.HasShutdownFinished)
+                {
+                    break;
+                }
+
+                // HACK: Simulate "DoEvents"
+                this.Dispatcher.Invoke(
+                    DispatcherPriority.Background,
+                    new ThreadStart(delegate { }));
+                Thread.Sleep(20);
+            }
+
+        }
+
+        private void HideHandlerDialog()
+        {
+            _hideRequest = true;
+            Visibility = Visibility.Hidden;
+            //_parent.IsEnabled = true;
+        }
+    }
+}
--- VideoTherapy/VideoTherapy/MainWindow.xaml.cs
+++ VideoTherapy/VideoTherapy/MainWindow.xaml.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,6 +11,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Windows.Threading;
+
 
 namespace VideoTherapy
 {
@@ -20,9 +22,30 @@ namespace VideoTherapy
     /// </summary>
     public partial class MainWindow : Window
     {
+        private bool authenticated = false;
+
         public MainWindow()
         {
             InitializeComponent();
+            
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // todo check if user is set the check box to "Rememeber me"
+            
+            if (!authenticated)
+            {
+                //LoginDialog.ShowHandlerDialog();
+                DataContext = new LoginDialog();
+
+            }
+            else
+            {
+                // todo load treatment window
+            }
+
         }
     }
 }
